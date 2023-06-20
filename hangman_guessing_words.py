31a794e53695274e26822363b164cd4c6f5ce51f
yateeshapappala <yateeshapappala@gmail.com>
2023-06-20 20:46:23 +0530

    guessing words for hangman game

---
+++ hangman_guessing_words.py
@@ -0,0 +1,4 @@
+words=['Bridge', 'Detail', 'Client', 'Search', 'Master', 'Dinner', 'Agency', 'Manner', 'Favour', 'Crisis', 'Prince', 'Output', 'Middle', 'Player', 'Threat', 'Notice', 'Bottom', 'Profit',
+       'Second', 'Option', 'Reform', 'Spring', 'Estate', 'Volume', 'Martin', 'Branch', 'Object', 'Driver', 'Belief', 'Murder', 'Flight', 'Treaty', 'Desire', 'Palace', 'Engine', 'Breath',
+       'Screen', 'Silver', 'Injury', 'Valley']
+
